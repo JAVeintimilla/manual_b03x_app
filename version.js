66381ff45a5cfd65ad13d2c49a70ff0e11998411
Toni Veintimilla <veintimilla.toni@gmail.com>
2026-09-23 18:15:41 +0200

    version 1.7.0

--- version.js
+++ version.js
@@ -1,2 +1,2 @@
 // Única fuente de la versión de la app: cámbiala aquí en cada actualización
-self.APP_VERSION_INFO = { version: "1.6.1", date: "2026-09-22" };
+self.APP_VERSION_INFO = { version: "1.7.0", date: "2026-09-23" };
